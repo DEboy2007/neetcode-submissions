dd5da46b4a36f91128637ccc54a037c32755e206
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2026-05-25 18:18:08 -0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            cout << mid << "\n";
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
