bad1a3a369b0f9d1ff492986e556d3aa93b03f1c
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2026-05-26 11:20:09 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,24 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<int>> mp;
+    unordered_map<string, unordered_map<int, string>> ts;
+    
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back(timestamp);
+        ts[key][timestamp] = value;
+    }
+    
+    string get(string key, int timestamp) {
+        if (mp[key].empty()) return "";
+        auto time = upper_bound(mp[key].begin(), mp[key].end(), timestamp);
+        if (time == mp[key].begin()) {
+            return "";
+        }
+        time--;
+        return ts[key][*time];
+    }
+};
